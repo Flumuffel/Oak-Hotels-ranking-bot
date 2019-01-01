38edc4e7e0d6fe6cfebd1871f8ce6264b5680f73
Flumuffel <luca.weber.gelti@gmail.com>
2019-01-01 17:47:06 +0100

    setup

---
+++ bot.js
@@ -0,0 +1,76 @@
+// this code was given to me by 1988_YumChocolate from the ROBLOX API Server, all credits (as far as I know) go to him
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+
+const roblox = require('noblox.js')
+const Discord = require('discord.js')
+const client = new Discord.Client();
+var token = "NTI5MDgxNzgwODUxMTc5NTQ3.DwuVXw.4WFK1KWszYPU9m8n_iC3a9GzGzI";
+
+client.login(token)
+
+var cookie = "_|WARNING:-DO-NOT-SHARE-THIS.--Sharing-this-will-allow-someone-to-log-in-as-you-and-to-steal-your-ROBUX-and-items.|_67814DBEDDD474CC629B1FD87C91DCECA7DA26BFECD659F1F5155CC9ED3A43FC15D86E3C7C948BA2C1A769953860AB04871EE435A1F207F91EA28D1FA943018140059ECABB0816EDB8FDDE29121C73CE807703ACB91A5D0EE6EBA9CE4737B21B5BC97841E6057A76E252A5D4E8DEE6B7F2361730EA1FF92E466BF8258BBB1AD483493E907FCCBAAE594BF9B6F61380AAD7FD88288E3748CE43EE69953A3BB74437B36C2BCE8150C6D32D2A72D75F49D6CB14DD8A604B9E21ACAF82D050B7884DB4505264419902EADA1FBD0FA257E654FF01C170DF9E2B6C35B5C5640F592965487F055F97F4C0888F218CC453CCDC686DBE9E320CC4FEC0771421A25E30ADCDB61E45F6283E2ADFA40C8408CAC1C84F7291C3F657A0AB3A09092678FADD8C9D6D1F3DD1";
+
+var prefix = 'o!';
+var groupId = 4560058;
+var maximumRank = 250;
+
+function login() {
+    return roblox.cookieLogin(cookie);
+}
+
+login() // Log into ROBLOX
+    .then(function() { // After the function has been executed
+        console.log('Logged in.') // Log to the console that we've logged in
+    })
+    .catch(function(error) { // This is a catch in the case that there's an error. Not using this will result in an unhandled rejection error.
+        console.log(`Login error: ${error}`) // Log the error to console if there is one.
+    });
+ 
+function isCommand(command, message){
+    var command = command.toLowerCase();
+    var content = message.content.toLowerCase();
+    return content.startsWith(prefix + command);
+}
+ 
+client.on('message', (message) => {
+    if (message.author.bot) return; // Dont answer yourself.
+    var args = message.content.split(/[ ]+/)
+   
+    if(isCommand('setrank', message)){
+       if(!message.member.roles.some(r=>["Ranking Permissions"].includes(r.name)) ) // OPTIONAL - Checks if the sender has the specified roles to carry on further
+        return message.reply("You don't have the permission to use the Rank System.");
+        var username = args[1]
+        var rankIdentifier = Number(args[2]) ? Number(args[2]) : args[2];
+        if (!rankIdentifier) return message.channel.send("Please enter a rank");
+        if (username){
+            message.channel.send(`Checking ROBLOX for ${username}`)
+            roblox.getIdFromUsername(username)
+            .then(function(id){
+                roblox.getRankInGroup(groupId, id)
+                .then(function(rank){
+                    if(maximumRank <= rank){
+                        message.channel.send(`${id} is rank ${rank} and not promotable.`)
+                    } else {
+                        message.channel.send(`${id} is rank ${rank} and promotable.`)
+                        roblox.setRank(groupId, id, rankIdentifier)
+                        .then(function(newRole){
+                            message.channel.send(`Changed rank to ${newRole.Name}`)
+                        }).catch(function(err){
+                            console.error(err)
+                            message.channel.send("Failed to change rank.")
+                        });
+                    }
+                }).catch(function(err){
+                    message.channel.send("Couldn't get that player in the group.")
+                });
+            }).catch(function(err){
+                message.channel.send(`Sorry, but ${username} doesn't exist on ROBLOX.`)
+           });
+       } else {
+           message.channel.send("Please enter a username.")
+       }
+       return;
+   }
+   
+})
